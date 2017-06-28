69c16f697e6e2898b689daaabe6b9f6a229c8305
HeyImRige <Robert.aron.tx@verizon.net>
2017-06-28 18:43:46 -0500

    cleaning a bit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Components/App';
+import App from './component/App';
 import registerServiceWorker from './registerServiceWorker';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {createStore} from 'redux';
+
+const store = createStore();
 
 ReactDOM.render(
     <Router>
