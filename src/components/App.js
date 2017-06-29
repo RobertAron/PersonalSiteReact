a1351ea4d46d73d89e3c1fba07798c6ac89f26a5
HeyImRige <Robert.aron.tx@verizon.net>
2017-06-28 22:04:02 -0500

    cards working properly

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import ProjectCard from './ProjectCard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>FIRST COMMIT</h1>
+        <ProjectCard/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './component/App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {createStore} from 'redux';
-import allReducers from './reducers'
+import allReducers from './reducers';
 
 const store = createStore(allReducers);
 
