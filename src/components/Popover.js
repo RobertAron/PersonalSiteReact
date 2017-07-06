bfd4981d6ae0ffdae0601a121100403b0458bd19
HeyImRige <Robert.aron.tx@verizon.net>
2017-07-05 23:47:18 -0500

    Icons and links to external references now works

--- src/components/Popover.js
+++ src/components/Popover.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
 import Popover from 'material-ui/Popover';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
 import './ProjectCard.css';
-import RemoveRedEye from 'material-ui/svg-icons/image/remove-red-eye';
-import "./Popover.css"
 import FontIcon from 'material-ui/FontIcon';
+import {black, red900} from 'material-ui/styles/colors';
+
+
 
 export default class PopoverExampleSimple extends React.Component {
 
     constructor(props) {
         super(props);
-
         this.state = {
             open: false,
         };
@@ -35,20 +34,42 @@ export default class PopoverExampleSimple extends React.Component {
     };
 
     createMenuList() {
-        Object.keys(this.props.projectLinks).map((key, value) => {
-            let icon;
-            switch(key){
+        return(Object.keys(this.props.projectLinks).map((key, value) => {
+            switch (key) {
                 case "Github":
-                    icon = <test/>
-
+                    return (
+                        <MenuItem primaryText={key}
+                            key={key}
+                            leftIcon={<FontIcon className="fa fa-github" color={black}/>}
+                            href={this.props.projectLinks.Github}
+                            target="_blank" />
+                    )
+                case "Youtube":
+                    return (
+                        <MenuItem primaryText={key}
+                            key={key}
+                            leftIcon={<FontIcon className="fa fa-youtube" color={red900}/>}
+                            href={this.props.projectLinks.Youtube}
+                            target="_blank" />
+                    )
+                case "Devpost":
+                    return (
+                        <MenuItem primaryText={key}
+                            key={key}
+                            leftIcon={<img src='../../res/DevpostIcon.svg' alt="devpost"/>}
+                            href={this.props.projectLinks.Devpost}
+                            target="_blank" />
+                    )
+                default:
+                    return (
+                        <MenuItem primaryText={key}
+                            key={key}
+                            leftIcon={<FontIcon className="fa fa-globe" />}
+                            href={this.props.projectLinks.key}
+                            target="_blank" />
+                    )
             }
-            return (
-                <MenuItem primaryText={key}
-                    leftIcon={icon}
-                    href={this.props.projectLinks}
-                    target="_blank" />
-            )
-        })
+        }))
     }
 
     render() {
@@ -70,13 +91,7 @@ export default class PopoverExampleSimple extends React.Component {
                     onRequestClose={this.handleRequestClose}
                 >
                     <Menu>
-                        <MenuItem primaryText="Refresh"
-                            leftIcon={<FontIcon className="fa fa-youtube" />}
-                            href="https://github.com/callemall/material-ui"
-                            target="_blank" />
-                        <MenuItem primaryText="Help &amp; feedback" />
-                        <MenuItem primaryText="Settings" />
-                        <MenuItem primaryText="Sign out" />
+                        {this.createMenuList()}
                     </Menu>
                 </Popover>
             </div>
