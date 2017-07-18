987ec60d58503edd638253395b74083fa7d27c2e
HeyImRige <Robert.aron.tx@verizon.net>
2017-07-18 14:35:48 -0500

    about to change the format of projects

--- src/components/ProjectDetailsContent.js
+++ src/components/ProjectDetailsContent.js
@@ -7,7 +7,6 @@ class ProjectDetails extends Component {
     render() {
         console.log(this.props.project);
         return (
-
             <div>
                 <h1>Hello World From Details Content</h1>
                 <h2>{this.props.project.projectTitle}</h2>
