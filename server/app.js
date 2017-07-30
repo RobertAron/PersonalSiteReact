6752b6749a31857cbfa3d59cbe7c19d9f129c89d
HeyImRige <Robert.aron.tx@verizon.net>
2017-07-30 01:33:25 -0500

    backend can almost send email

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 // server/app.js
+const credentials =require('./credentials');
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const nodemailer = require('nodemailer');
 
 const app = express();
 
@@ -15,5 +17,39 @@ app.use(express.static(path.resolve(__dirname, '..', 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
+
+app.post('/api/sendmail',(req,res)=>{
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth:{
+      user: 'example@gmail.com',
+      pass: 'password'
+    }
+  })
+  const mailOptions = {
+    from: 'example@gmail.com>', // sender address
+    to: 'receiver@destination.com', // list of receivers
+    subject: 'Email Example', // Subject line
+    text: "text" //, // plaintext body
+  }
+  /*
+  transporter.sendMail(mailOptions,(error,info)=>{
+    if(error){
+      console.log("sending the email failed");
+      res.json({message:"an error occured"})
+    }
+    else{
+      console.log("sending the email succeeded");
+      res.json({message:"an error succeeded"})
+    }
+  })
+  */
+  console.log("inside of email api");
+  console.log(credentials);
+  res.send("Sent Email");
+})
+
+
+
 
 module.exports = app;
---
+++ server/credentials.js
@@ -0,0 +1,12 @@
+const credentials = {
+    //I'm not that dumb :P
+    password: 'test-password',
+    from: 'example@gmail.com>', // sender address
+    to: 'receiver@destination.com', // list of receivers
+    subject: 'Email Example', // Subject line
+    text: "text" //, // plaintext body
+}
+
+module.exports = {
+    credentials : credentials
+};
